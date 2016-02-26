a4a07c9949933c1fcc2693f545b6127dfa9bfbc3
hagibis <zhangxsh.qll@qq.com>
2016-02-26 15:52:30 +0800

    补充 注释

--- football.js
+++ football.js
@@ -15,23 +15,6 @@ var football = (function () {
 	var image
 	var ball
 
-	function Ball (ballimage, argument) {
-		this.width = argument.width
-		this.height = argument.height
-		this.x = argument.left
-		this.y = argument.top
-		this.image = ballimage
-
-		this.gravity = 0.4
-		this.vy = 0.8
-		this.vx = 4
-		this.vyAdjust = -15
-		this.vxAdjust = 0.25
-		this.factor = 0.65
-
-		this.end = false
-	}
-
 	Ball.prototype.draw = function(){
 		context.drawImage(this.image, 
 			0, 0, 
@@ -42,16 +25,19 @@ var football = (function () {
 	}
 
 	Ball.prototype.move = function(){
+		// y方向
 		this.y += this.vy
-		this.vy += this.gravity
+		this.vy += this.gravity // 实现加速下降的效果
 
+		// x方向
 		if (this.vx > 0) {
 			this.x += this.vx
 		}
 
+		// 判断是否发生碰撞
 		if ((this.y + this.height) > canvas.height) {
 			this.hit()
-			this.vyAdjust *= this.factor
+			this.vyAdjust *= this.factor // 调整校验参数
 			this.vx -= this.vxAdjust
 		}
 
@@ -80,6 +66,23 @@ var football = (function () {
 		if (!ball.end) {
 			requestAnimFrame(loop) 
 		}
+	}
+
+	function Ball (ballimage, argument) {
+		this.width = argument.width
+		this.height = argument.height
+		this.x = argument.left
+		this.y = argument.top
+		this.image = ballimage
+
+		// 自由落体相关系数
+		this.gravity = 0.4  // 重力加速度
+		this.vy = 0.8 // 垂直方向增量
+		this.vx = 4 // 水平方向增量
+		this.vyAdjust = -15 // 垂直方向阻尼系数（校验参数）
+		this.vxAdjust = 0.25 // 水平方向阻尼系数
+		this.factor = 0.65 // 衰减系数
+		this.end = false
 	}
 
 	function loadBall () {
